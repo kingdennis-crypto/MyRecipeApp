c152243959d829bf7cc0e4f1fa81605c0b55f61d
kingdennis-crypto <dennismoes@me.com>
2021-07-02 09:00:19 +0200

    Added a list for the ingredients

--- components/DetailScreen.js
+++ components/DetailScreen.js
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 export default function DetailScreen({ route }) {
   const { item } = route.params;
   console.log(item.label);
+  console.log(item.ingredients);
   return (
     <View>
       <Image
@@ -12,6 +13,18 @@ export default function DetailScreen({ route }) {
       />
       <View style={styles.textContainer}>
         <Text style={styles.titleText}>{item.label}</Text>
+        <Text>{item.dishType}</Text>
+        <ScrollView style={{ maxHeight: 300 }}>
+          {item.ingredients.map((data) => (
+            <View style={styles.itemList}>
+              <Image
+                source={{ uri: data.image }}
+                style={{ width: 50, height: 50 }}
+              />
+              <Text style={styles.ingredientText}>{data.text}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -47,5 +60,29 @@ const styles = StyleSheet.create({
     fontSize: 32,
     textAlign: "center",
     fontWeight: "400",
+  },
+
+  itemList: {
+    flexDirection: "row",
+    borderWidth: 1,
+    margin: 3,
+    borderRadius: 5,
+    alignItems: "center",
+    backgroundColor: "#FFF",
+
+    shadowRadius: 5,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowColor: "#000",
+    elevation: 4,
+    shadowOpacity: 0.2,
+  },
+
+  ingredientText: {
+    paddingHorizontal: 10,
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
