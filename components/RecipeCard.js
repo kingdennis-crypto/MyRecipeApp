1c000f318e7a421aab16e7c9541ff6cbe76bbea3
kingdennis-crypto <dennismoes@me.com>
2021-07-03 14:13:44 +0200

    Tidied some things up

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, Image, TouchableOpacity, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import uuid from "react-native-uuid";
 
@@ -8,23 +8,27 @@ export default function RecipeCard(props) {
   return (
     <TouchableOpacity
       style={styles.cardItem}
-      key={uuid.v4()}
       onPress={() => navigation.navigate("Detail", { item: props.recipe })}
     >
       <Image
-        key={uuid.v4()}
         source={{ uri: props.recipe.image }}
-        style={{ width: "100%", height: "70%" }}
+        style={{
+          width: "100%",
+          height: 80,
+          borderRadius: 15,
+        }}
       />
-      <Text key={uuid.v4()} style={styles.recipeTitle}>
-        {props.recipe.label}
-      </Text>
-      <Text key={uuid.v4()} style={styles.mealTypeText}>
-        {props.recipe.mealType}
-      </Text>
-      <Text key={uuid.v4()} style={styles.mealTypeText}>
-        {props.recipe.cuisineType}
-      </Text>
+      <Text style={styles.recipeTitle}>{props.recipe.label}</Text>
+      {props.recipe.cuisineType === undefined ? (
+        <Text style={styles.mealTypeText}>No Cuisine Type</Text>
+      ) : (
+        <Text style={styles.cuisineTypeText}>{props.recipe.cuisineType}</Text>
+      )}
+      {props.recipe.mealType === undefined ? (
+        <Text style={styles.mealTypeText}>No Meal Type</Text>
+      ) : (
+        <Text style={styles.mealTypeText}>{props.recipe.mealType}</Text>
+      )}
     </TouchableOpacity>
   );
 }
@@ -34,8 +38,8 @@ const styles = StyleSheet.create({
     width: "45%",
     backgroundColor: "#FFF",
     margin: 5,
-    borderRadius: 5,
-    height: 250,
+    borderRadius: 15,
+    padding: 10,
 
     shadowColor: "#000",
     shadowOffset: {
@@ -51,7 +55,15 @@ const styles = StyleSheet.create({
     width: "100%",
     textAlign: "center",
     fontSize: 14,
-    padding: 5,
+    fontWeight: "500",
+    margin: 3,
+  },
+
+  cuisineTypeText: {
+    alignSelf: "center",
+    fontSize: 12,
+    padding: 3,
+    borderRadius: 5,
   },
 
   mealTypeText: {
