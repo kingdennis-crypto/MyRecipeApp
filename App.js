d0863319b87ecdc5b1bcc1c3ec5f2bb3d805b596
kingdennis-crypto <dennismoes@me.com>
2021-07-05 16:04:47 +0200

    Added the link to a settings page

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from "./components/HomeScreen";
-import DetailScreen from "./components/DetailScreen";
+import DetailScreen from "./components/DetailScreen/DetailScreen";
+import SettingScreen from "./components/SettingScreen/SettingScreen";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -22,6 +23,7 @@ export default function App() {
           component={DetailScreen}
           options={{ headerShown: false }}
         />
+        <Stack.Screen name="Settings" component={SettingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
