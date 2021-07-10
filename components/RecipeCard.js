86ff26c66604a1c0fcb12247f0bce565fba45f9d
kingdennis-crypto <dennismoes@me.com>
2021-07-10 11:50:22 +0200

    Changed the innerworkings of the recipe card and radically changed the design

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, StyleSheet, Image, TouchableOpacity, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import uuid from "react-native-uuid";
 
 export default function RecipeCard(props) {
@@ -10,29 +11,23 @@ export default function RecipeCard(props) {
       style={styles.cardItem}
       onPress={() => navigation.navigate("Detail", { item: props.recipe })}
     >
-      <Image
-        source={{ uri: props.recipe.image }}
-        style={{
-          width: "100%",
-          height: 80,
-          borderTopLeftRadius: 10,
-          borderTopRightRadius: 10,
-        }}
-      />
-      <View style={styles.mealTypeContainer}>
-        {props.recipe.mealType === undefined ? (
-          <Text style={styles.mealTypeText}>No Meal Type</Text>
-        ) : (
-          <Text style={styles.mealTypeText}>{props.recipe.mealType}</Text>
-        )}
-      </View>
+      <Image source={{ uri: props.recipe.image }} style={styles.cardImage} />
       <View style={styles.textContainer}>
-        <Text style={styles.recipeTitle}>{props.recipe.label}</Text>
-        {props.recipe.cuisineType === undefined ? (
-          <Text style={styles.mealTypeText}>No Cuisine Type</Text>
+        {props.recipe.label.length > 15 ? (
+          <Text>{props.recipe.label.substring(0, 15)}...</Text>
         ) : (
-          <Text style={styles.cuisineTypeText}>{props.recipe.cuisineType}</Text>
+          <Text>{props.recipe.label}</Text>
         )}
+        <View style={styles.detailContainer}>
+          <Ionicons name="time-outline" size={14} color="#000" />
+          <Text style={{ marginLeft: 3 }}>{props.recipe.totalTime} min</Text>
+        </View>
+        <View style={styles.detailContainer}>
+          <Ionicons name="flame-outline" size={14} color="#000" />
+          <Text style={{ marginLeft: 3 }}>
+            {Math.floor(props.recipe.calories)} kcal
+          </Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
@@ -41,9 +36,9 @@ export default function RecipeCard(props) {
 const styles = StyleSheet.create({
   cardItem: {
     width: "45%",
+    height: 300,
     backgroundColor: "#FFF",
     margin: 5,
-    borderRadius: 10,
 
     shadowColor: "#000",
     shadowOffset: {
@@ -55,8 +50,33 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
 
+  cardImage: {
+    width: "100%",
+    height: "100%",
+  },
+
   textContainer: {
-    padding: 5,
+    position: "absolute",
+    bottom: 7,
+    left: 7,
+    right: 7,
+    backgroundColor: "rgb(255,255,255)",
+    padding: 7,
+
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowRadius: 6,
+    elevation: 5,
+  },
+
+  detailContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
 
   recipeTitle: {
