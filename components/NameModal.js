ab800f6685e6dc4d05b0ea5dfcf017a03eb51332
kingdennis-crypto <dennismoes@me.com>
2021-07-13 16:34:30 +0200

    Changed the design of the modal and when on focus with the textinput and exits the modal it will release the focus

--- components/NameModal.js
+++ components/NameModal.js
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  Pressable,
+} from "react-native";
 import Modal from "react-native-modal";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // https://github.com/react-native-modal/react-native-modal
+
+// #051e3e
+// #251e3e
+// #451e3e
+// #651e3e
+// #851e3e
 
 export default function NameModal(props) {
   const [name, setName] = useState("");
@@ -33,23 +46,27 @@ export default function NameModal(props) {
     <View>
       <Modal
         isVisible={props.isVisible}
-        onBackdropPress={() => props.setIsVisible(false)}
+        onBackdropPress={() => {
+          props.setIsVisible(false);
+          setIsFocused(false);
+        }}
       >
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#FFF",
-            marginVertical: 200,
-            borderRadius: 7,
-          }}
-        >
+        <View style={styles.modalContainer}>
           <View style={styles.titleContainer}>
-            <Text style={styles.titleText}>Please change your name</Text>
-            <Text style={styles.oldNameText}>Old Name: {props.name}</Text>
+            <Text style={{ fontSize: 26, fontWeight: "600", marginBottom: 3 }}>
+              Please change your name
+            </Text>
+            <Text
+              style={{
+                textTransform: "uppercase",
+                color: "rgba(0,0,0,0.8)",
+                fontWeight: "500",
+              }}
+            >
+              Old Name: {props.name}
+            </Text>
           </View>
-          <View style={styles.textInputContainer}>
+          <View>
             <TextInput
               onFocus={() => setIsFocused(!isFocused)}
               onEndEditing={() => setIsFocused(!isFocused)}
@@ -60,7 +77,9 @@ export default function NameModal(props) {
               value={name}
               placeholder="Enter your name"
             />
-            <Button title="Change Name" onPress={StoreName} />
+            <Pressable style={styles.submitButton}>
+              <Button title="Change Name" onPress={StoreName} color="#FFF" />
+            </Pressable>
           </View>
         </View>
       </Modal>
@@ -69,26 +88,11 @@ export default function NameModal(props) {
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    position: "absolute",
-    top: 10,
-    left: 10,
-    width: "100%",
-  },
-
-  titleText: {
-    fontSize: 28,
-    fontWeight: "500",
-  },
-
-  oldNameText: {
-    textTransform: "uppercase",
-    fontSize: 16,
-    color: "rgba(0,0,0,0.5)",
-  },
-
-  textInputContainer: {
-    width: "90%",
+  modalContainer: {
+    backgroundColor: "#FFF",
+    borderRadius: 7,
+    height: "50%",
+    padding: 10,
   },
 
   textInputStyle: {
@@ -102,8 +106,19 @@ const styles = StyleSheet.create({
   focusedTextInput: {
     padding: 10,
     borderWidth: 1,
-    borderColor: "rgb(200,170,130)",
+    borderColor: "#851e3e",
     borderRadius: 7,
     backgroundColor: "#E4E4E4",
+  },
+
+  titleContainer: {
+    marginBottom: 15,
+  },
+
+  submitButton: {
+    padding: 5,
+    marginTop: 10,
+    backgroundColor: "#051e3e",
+    borderRadius: 7,
   },
 });
