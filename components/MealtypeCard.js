6f7c04d57e1f019a495a4c2671cd18e7fa989e44
kingdennis-crypto <dennismoes@me.com>
2021-07-14 15:57:21 +0200

    Added the dark mode functionality

--- components/MealtypeCard.js
+++ components/MealtypeCard.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity, Text, useColorScheme } from "react-native";
 import styles from "./HomeScreen.style.light";
+import darkStyles from "./HomeScreen.style.dark";
 
 export default function MealtypeCard(props) {
+  const [isDark, setIsDark] = React.useState(colorScheme === "dark");
+  const colorScheme = useColorScheme();
+  const themeStyle = isDark ? darkStyles : styles;
+
+  React.useEffect(() => {
+    setIsDark(colorScheme === "dark");
+  }, [colorScheme]);
+
   return (
     <TouchableOpacity onPress={() => props.changeMealType(props.name)}>
       <Text
         style={
           props.mealTypes === props.name
-            ? styles.focusedMealType
-            : styles.normalMealType
+            ? themeStyle.focusedMealType
+            : themeStyle.normalMealType
         }
       >
         {props.name}
