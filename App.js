b8b264ffa839461a5f24fcc3c2b8f2207fa81626
kingdennis-crypto <dennismoes@me.com>
2021-07-14 15:40:19 +0200

    Added a appearanceprovider to help for dark mode

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import { AppearanceProvider } from "react-native-appearance";
+
 import HomeScreen from "./components/HomeScreen";
 import DetailScreen from "./components/DetailScreen/DetailScreen";
 import RecipePage from "./components/RecipePage";
@@ -10,20 +12,22 @@ export default function App() {
   const Stack = createStackNavigator();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Detail"
-          component={DetailScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="RecipePage" component={RecipePage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppearanceProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Detail"
+            component={DetailScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="RecipePage" component={RecipePage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppearanceProvider>
   );
 }
