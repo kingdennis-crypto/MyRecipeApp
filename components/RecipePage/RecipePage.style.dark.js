df9013260dfd36e4fc7c622aea91c342228efe41
kingdennis-crypto <dennismoes@me.com>
2021-07-16 15:02:53 +0200

    Reworked both stylesheets

--- components/RecipePage/RecipePage.style.dark.js
+++ components/RecipePage/RecipePage.style.dark.js
@@ -3,81 +3,38 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   recipeBody: {
     flex: 1,
-    backgroundColor: "#3c2f2f",
-  },
-
-  recipeImage: {
-    alignSelf: "stretch",
-    height: 250,
-  },
-
-  titleContainer: {
-    padding: 10,
-    marginBottom: 20,
-  },
-
-  titleText: {
-    fontSize: 48,
-    fontWeight: "500",
-    color: "#fff4e6",
   },
 
   container: {
+    marginTop: -25,
     backgroundColor: "#3c2f2f",
-    height: "100%",
-    width: "100%",
+    borderRadius: 25,
+    padding: 15,
+    flex: 1,
   },
 
-  textContainer: {
-    width: "100%",
-    alignItems: "center",
-    marginTop: -50,
-  },
-
-  textContainer2: {
-    marginBottom: 10,
-    borderRadius: 7,
-
-    backgroundColor: "#4b3832",
-    width: "95%",
-    padding: 10,
-
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-    elevation: 5,
-  },
-
-  titleText: {
-    fontSize: 22,
+  titleStyle: {
+    fontSize: 26,
     fontWeight: "500",
   },
 
-  ingredientsContainer: {
+  descriptionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginVertical: 10,
-
-    backgroundColor: "#FFF",
-    width: "90%",
-    padding: 10,
-
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-    elevation: 5,
   },
 
-  ingredientsTitle: {
-    fontSize: 18,
-    fontWeight: "500",
+  descriptionItem: {
+    padding: 7,
+    backgroundColor: "#854442",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  descriptionText: {
+    color: "#be9b7b",
+    fontWeight: "bold",
+    textTransform: "uppercase",
   },
 });
--- components/RecipePage/RecipePage.style.light.js
+++ components/RecipePage/RecipePage.style.light.js
@@ -3,83 +3,38 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   recipeBody: {
     flex: 1,
-    backgroundColor: "#fff4e6",
-  },
-
-  recipeImage: {
-    alignSelf: "stretch",
-    height: 250,
-  },
-
-  titleContainer: {
-    padding: 10,
-    marginBottom: 20,
-  },
-
-  titleText: {
-    fontSize: 66,
-    fontWeight: "500",
-    color: "#3c2f2f",
   },
 
   container: {
+    marginTop: -25,
     backgroundColor: "#fff4e6",
-    height: "100%",
-    width: "100%",
+    borderRadius: 25,
+    padding: 15,
+    flex: 1,
   },
 
-  textContainer: {
-    width: "100%",
-    alignItems: "center",
-    marginTop: -50,
-  },
-
-  textContainer2: {
-    marginBottom: 10,
-    borderRadius: 7,
-
-    backgroundColor: "#f2dec4",
-    width: "95%",
-    padding: 10,
-
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-    elevation: 5,
-  },
-
-  titleText: {
-    fontSize: 22,
+  titleStyle: {
+    fontSize: 26,
     fontWeight: "500",
-    color: "#3c2f2f",
   },
 
-  ingredientsContainer: {
+  descriptionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginVertical: 10,
-
-    backgroundColor: "#f2dec4",
-    width: "90%",
-    padding: 10,
-
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-    elevation: 5,
   },
 
-  ingredientsTitle: {
-    fontSize: 18,
-    fontWeight: "500",
-    color: "#3c2f2f",
+  descriptionItem: {
+    padding: 7,
+    backgroundColor: "#be9b7b",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  descriptionText: {
+    color: "#854442",
+    fontWeight: "bold",
+    textTransform: "uppercase",
   },
 });
