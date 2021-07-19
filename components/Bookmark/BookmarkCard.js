26255875917fef114f9058408c5846bcab1067d7
kingdennis-crypto <dennismoes@me.com>
2021-07-19 15:45:11 +0200

    First Commits to make a bookmark function

---
+++ components/Bookmark/BookmarkCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function BookmarkCard() {
+  return (
+    <View style={{ borderWidth: 1 }}>
+      <Text>Bookmark</Text>
+    </View>
+  );
+}
---
+++ components/FAB.js
@@ -0,0 +1,41 @@
+import { faBaby } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
+
+export default function FAB() {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      style={{
+        position: "absolute",
+        bottom: 35,
+        right: 35,
+        backgroundColor: "rgb(255,255,255)",
+        borderRadius: 25,
+        width: 65,
+        height: 65,
+        zIndex: 10,
+        justifyContent: "center",
+        alignItems: "center",
+
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+      }}
+      onPress={() => {
+        navigation.navigate("BookmarkPage");
+      }}
+    >
+      <Icon name="bookmark-outline" size={38} color="#000" />
+    </TouchableOpacity>
+  );
+}
