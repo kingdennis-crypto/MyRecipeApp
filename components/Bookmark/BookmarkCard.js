349d2aea265cfeb91f8623965d5404cdc528b8b3
kingdennis-crypto <dennismoes@me.com>
2021-07-20 17:02:48 +0200

    Reworked on the page and removed the stylesheet

--- components/Bookmark/BookmarkCard.js
+++ components/Bookmark/BookmarkCard.js
@@ -1,76 +1,43 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  themeStyleheet,
+  TouchableOpacity,
+  useColorScheme,
+} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
 
-export default function BookmarkCard() {
+import darkStyle from "./style.dark";
+import lightStyle from "./style.light";
+
+export default function BookmarkCard(props) {
+  const [isDark, setIsDark] = useState(colorScheme === "dark");
+
+  const colorScheme = useColorScheme();
+  const themeStyle = isDark ? darkStyle : lightStyle;
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setIsDark(colorScheme === "dark");
+  }, [colorScheme]);
+
   return (
-    <View style={styles.container}>
-      <View style={styles.textItem}>
-        <Text style={styles.nameText}>Title</Text>
-        <Text style={styles.typeText}>Lunch</Text>
+    <View style={themeStyle.cardItem}>
+      <View style={themeStyle.textItem}>
+        <Text style={themeStyle.nameText}>{props.item.label}</Text>
+        <Text style={themeStyle.typeText}>{props.item.mealType}</Text>
       </View>
-      <TouchableOpacity style={styles.bookmarkItem}>
+      {/* <TouchableOpacity style={themeStyle.bookmarkItem}>
         <Icon name="bookmark-outline" size={32} color="#000" />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.chevronItem}>
-        <Icon name="chevron-forward-outline" size={32} color="#000" />
+      </TouchableOpacity> */}
+      <TouchableOpacity
+        style={themeStyle.chevronItem}
+        onPress={() => navigation.navigate("RecipePage", { item: props.item })}
+      >
+        <Icon name="chevron-forward-outline" size={32} color="#3c2f2f" />
       </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignSelf: "stretch",
-    padding: 10,
-    marginHorizontal: 10,
-    marginVertical: 5,
-    borderRadius: 7,
-    backgroundColor: "#FFF",
-    flexDirection: "row",
-    alignItems: "center",
-
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-    elevation: 5,
-  },
-
-  nameText: {
-    fontSize: 18,
-    fontWeight: "500",
-  },
-
-  typeText: {
-    fontWeight: "300",
-  },
-
-  textItem: {
-    flex: 1,
-    backgroundColor: "rgba(0,0,0,0.3)",
-    marginHorizontal: 5,
-  },
-
-  bookmarkItem: {
-    width: 50,
-    backgroundColor: "rgba(0,0,0,0.3)",
-    marginHorizontal: 5,
-    alignItems: "center",
-    padding: 5,
-    borderRadius: 7,
-  },
-
-  chevronItem: {
-    width: 50,
-    backgroundColor: "rgba(0,0,0,0.3)",
-    marginHorizontal: 5,
-    alignItems: "center",
-    padding: 5,
-    borderRadius: 7,
-  },
-});
