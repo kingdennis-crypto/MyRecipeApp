6168e4b9e1047d1a375ac7b110a48cb482b274fd
kingdennis-crypto <dennismoes@me.com>
2021-07-21 09:01:30 +0200

    Changed the App.js file to implement a splash screen to check if it is the users first time

--- App.js
+++ App.js
@@ -1,43 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { AppearanceProvider } from "react-native-appearance";
+import * as SplashScreen from "expo-splash-screen";
 
 import HomeScreen from "./components/HomeScreen";
 import DetailScreen from "./components/DetailScreen/DetailScreen";
 import RecipePage from "./components/RecipePage";
 import BookmarkPage from "./components/Bookmark/BookmarkPage";
+import SetupScreen from "./components/SetupScreen";
 
 export default function App() {
   const Stack = createStackNavigator();
+  const [firstTime, setFirstTime] = useState(true);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  async function checkSetupScreen() {
+    try {
+      const value = await AsyncStorage.getItem("@FirstTime");
+      console.log(value);
+      if (value === "yes") {
+        setFirstTime(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await checkSetupScreen();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (appIsReady) {
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
-    <AppearanceProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {firstTime && (
           <Stack.Screen
-            name="Home"
-            component={HomeScreen}
+            name="FirstTime"
+            component={SetupScreen}
             options={{ headerShown: false }}
           />
-          <Stack.Screen
-            name="Detail"
-            component={DetailScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="RecipePage"
-            component={RecipePage}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="BookmarkPage"
-            component={BookmarkPage}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AppearanceProvider>
+        )}
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RecipePage"
+          component={RecipePage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="BookmarkPage"
+          component={BookmarkPage}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
